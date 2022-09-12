8867f03d05ef87bd3828293398670bbe4d7b3cee
Eli Fuchsman <efuchsman@gmail.com>
2022-09-11 23:53:44 -0600

    Create test for Renter attributes

--- spec/renter_spec.rb
+++ spec/renter_spec.rb
@@ -9,4 +9,10 @@ RSpec.describe Renter do
   expect(renter).to be_an_instance_of(Renter)
   end
 
+  it 'has readable attributes' do
+    renter = Renter.new("Patrick Star", "4242424242424242")
+
+    expect(renter.name).to eq("Patrick Star")
+    expect(renter.credit_card_number).to eq("4242424242424242")
+  end
 end
