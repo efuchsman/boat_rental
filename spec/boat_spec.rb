39b34a121bad24c27bebe28f7f374c27d4dba0f8
Eli Fuchsman <efuchsman@gmail.com>
2022-09-11 23:47:59 -0600

    Create test for rental hours

--- spec/boat_spec.rb
+++ spec/boat_spec.rb
@@ -8,5 +8,20 @@ RSpec.describe Boat do
   expect(kayak).to be_an_instance_of(Boat)
 end
 
+  it 'has readable attributes' do
+    kayak = Boat.new(:kayak, 20)
+
+    expect(kayak.type).to eq(:kayak)
+    expect(kayak.price_per_hour).to eq(20)
+  end
+
+  it "can be rented" do
+    kayak = Boat.new(:kayak, 20)
+
+    expect(kayak.hours_rented).to eq(0)
+    kayak.add_hour
+
+    expect(kayak.hours_rented).to eq(1)
+  end
 
 end
