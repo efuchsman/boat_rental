bf41d7fd3ad61613ad4dcd373207f6445428f8a6
Eli Fuchsman <efuchsman@gmail.com>
2022-09-13 17:14:51 -0600

    write method for charging the boat

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,14 +1,36 @@
 class Dock
-attr_reader :name, :max_rental_time, :rental_log
+  attr_reader :name, :max_rental_time, :rental_log
+
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
-    @rental_log ={}
+    @rental_log = {}
   end
 
   def rent(boat, renter)
+    @renter = renter
     @rental_log[boat] = renter
   end
 
+  def rental_amount(boat)
+    @boat = boat
+    boat.price_per_hour * boat.hours_rented
+  end
 
+  def card_number(boat)
+    @rental_log[boat].credit_card_number
+  end
+
+
+  def charge(boat)
+
+  @boat = boat
+  hash = {
+    :card_number => card_number(boat),
+    :amount => rental_amount(boat)
+
+  }
+
+  end
+  hash
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -37,4 +37,32 @@ RSpec.describe Dock do
     )
   end
 
+  it "can charge renters" do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+
+    kayak_1.add_hour
+    kayak_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+
+
+    dock.charge(kayak_1)
+    dock.charge(sup_1)
+
+    expect(dock.charge(kayak_1)).to eq({:card_number => "4242424242424242",
+      :amount => 40})
+      expect(dock.charge(sup_1)).to eq({:card_number => "1313131313131313", :amount => 45})
+
+  end
+
 end
