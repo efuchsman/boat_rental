aa8b251cdb3c4cdb02700411504afbeb4eeae593
Eli Fuchsman <efuchsman@gmail.com>
2022-09-12 22:33:29 -0600

    Create spec for rentals

--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -16,4 +16,25 @@ RSpec.describe Dock do
     expect(dock.max_rental_time).to eq(3)
   end
 
+  it 'can rent to renters' do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+    dock.rent(sup_1, eugene)
+
+    expect(dock.rental_log).to eq(
+      {
+        kayak_1 => patrick,
+        kayak_2 => patrick,
+        sup_1 => eugene
+      }
+    )
+  end
+
 end
