bdf99647dfc9dee2cb323a027b0660496aad0252
Eli Fuchsman <efuchsman@gmail.com>
2022-09-13 18:02:53 -0600

    Create test for logging hours

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,5 +1,5 @@
 class Dock
-  attr_reader :name, :max_rental_time, :rental_log
+  attr_reader :name, :max_rental_time, :rental_log, :revenue
 
   def initialize(name, max_rental_time)
     @name = name
@@ -12,29 +12,34 @@ class Dock
     @rental_log[boat] = renter
   end
 
-  def rental_amount(boat)
-    @boat = boat
-    if boat.hours_rented < @max_rental_time
-    boat.price_per_hour * boat.hours_rented
-    else
-      boat.price_per_hour * @max_rental_time
-    end
-  end
-
-  def card_number(boat)
-    @rental_log[boat].credit_card_number
-  end
 
 
   def charge(boat)
 
   @boat = boat
+
+    if boat.hours_rented < @max_rental_time
+      amount = boat.price_per_hour * boat.hours_rented
+     else
+      amount = boat.price_per_hour * @max_rental_time
+    end
+
   hash = {
-    :card_number => card_number(boat),
-    :amount => rental_amount(boat)
+    :card_number => @rental_log[boat].credit_card_number,
+    :amount => amount
 
   }
 
   end
-  hash
+
+  def log_hour
+    @rental_log.each_key do |boat|
+      boat.add_hour
+    end
+  end
+
+
+
+
+
 end
--- spec/dock_spec.rb
+++ spec/dock_spec.rb
@@ -70,4 +70,33 @@ RSpec.describe Dock do
 
   end
 
+  it "can log hours" do
+    dock = Dock.new("The Rowing Dock", 3)
+    kayak_1 = Boat.new(:kayak, 20)
+    kayak_2 = Boat.new(:kayak, 20)
+    canoe = Boat.new(:canoe, 25)
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    sup_2 = Boat.new(:standup_paddle_board, 15)
+    patrick = Renter.new("Patrick Star", "4242424242424242")
+    eugene = Renter.new("Eugene Crabs", "1313131313131313")
+
+    dock.rent(kayak_1, patrick)
+    dock.rent(kayak_2, patrick)
+
+    dock.log_hour
+
+
+    expect(kayak_1.hours_rented).to eq(1)
+    expect(kayak_2.hours_rented).to eq(1)
+
+    dock.rent(canoe, patrick)
+
+    dock.log_hour
+    expect(kayak_1.hours_rented).to eq(2)
+    expect(kayak_2.hours_rented).to eq(2)
+    expect(canoe.hours_rented).to eq(1)
+
+
+  end
+
 end
